de688537dcbfe9be6a170ba7008733f0984588e2
Денис Орехов <103766080+DenOrekhov567@users.noreply.github.com>
2023-07-03 18:47:57 +0500

    Add files via upload
    Добавлен первый урок

---
+++ DPA/Practice/DPALesson1/DPALesson1/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * 
+ * Изучаемые вопросы:
+ * точка входа программы; 1
+ * инструкции; 5
+ * регистрозависимость; 6
+ * комментарии; 2
+ * файл проекта; 3
+ * 
+ * перменные; 4
+ * константы; 7
+ * литералы; 8
+ * константы. 9
+ * 
+ */
+
+namespace DPALesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // Переменные
+            string name;
+            name = "Tom";
+
+            // Регистрозависимость
+            //Name = "Tom2"; // Не работает
+
+            // Литералы
+            string newName = "Jesica";
+
+            // Константы
+            const double PI = 3.14;
+        }
+    }
+}
